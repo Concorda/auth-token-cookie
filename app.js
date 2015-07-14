7e2e9808636db377ec70ec5e7d3a772ca6056e22
Mircea Alexandru <mircea.alexandru@gmail.com>
2015-07-14 13:08:35 +0300

    change command patterns

--- app.js
+++ app.js
@@ -4,23 +4,24 @@ module.exports = function ( options ) {
   var seneca = this
   var plugin = 'seneca-auth-token-plugin'
 
-  seneca.add({role: 'token', cmd: 'set'}, function(args, cb){
+  function setToken(args, cb){
     var tokenkey  = args.tokenkey
     var token     = args.token
     var res       = args.res
-    var req       = args.req
 
     res.seneca.cookies.set( tokenkey, token )
 
     cb(null, {token: token})
-  })
+  }
 
-  seneca.add({role: 'token', cmd: 'get'}, function(args, cb){
+  function getToken(args, cb){
     var tokenkey  = args.tokenkey
-    var res       = args.res
     var req       = args.req
     cb(null, {token: req.seneca.cookies.get( tokenkey )})
-  })
+  }
+
+  seneca.add({role: 'auth', set: 'token'}, setToken)
+  seneca.add({role: 'auth', get: 'token'}, getToken)
 
   return {
     name:plugin
