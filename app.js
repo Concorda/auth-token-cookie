85a31492dfffe10081f4edf7c8f75096943c170e
Mircea Alexandru <mircea.alexandru@gmail.com>
2015-07-16 17:05:42 +0300

    moved the tokenkey as optional parameter in this plugin

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 "use strict";
+var _ = require('lodash')
+var default_options = require('./default-options.js')
 
 module.exports = function ( options ) {
   var seneca = this
   var plugin = 'seneca-auth-token-plugin'
+  options = _.extend({}, default_options, options)
 
   function setToken(args, cb){
-    var tokenkey  = args.tokenkey
+    var tokenkey  = args.tokenkey || options.tokenkey
     var token     = args.token
     var res       = args.res
 
@@ -15,7 +18,7 @@ module.exports = function ( options ) {
   }
 
   function getToken(args, cb){
-    var tokenkey  = args.tokenkey
+    var tokenkey  = args.tokenkey || options.tokenkey
     var req       = args.req
     cb(null, {token: req.seneca.cookies.get( tokenkey )})
   }
