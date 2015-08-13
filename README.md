6de96d0a522ec19324af1d0dd477f3bd2088a882
mirceaalexandru <mircea.alexandru@gmail.com>
2015-08-13 17:37:26 +0300

    change module to be a simple module, not seneca plugin.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-seneca-auth-token-cookie
+auth-token-cookie
 ========================
 
-[Seneca](http://senecajs.org) plugin for storing auth token as cookie
+[Seneca](http://senecajs.org) internal module for storing auth token as cookie
 
-This plugin is used by [seneca-auth](https://www.npmjs.com/package/seneca-auth)
+This module is used by [seneca-auth](https://www.npmjs.com/package/seneca-auth)
 
 For a gentle introduction to Seneca itself, see the [senecajs.org](http://senecajs.org) site.
 
 ## Setup
-No setup is required, this plugin is loaded by default by the [seneca-auth](https://www.npmjs.com/package/seneca-auth).
+No setup is required, this module is loaded by default by the [seneca-auth](https://www.npmjs.com/package/seneca-auth).
--- app.js
+++ app.js
@@ -1,44 +1,45 @@
 "use strict";
+
+// external modules
 var _ = require('lodash')
-var default_options = require('./default-options.js')
 var Cookies       = require('cookies')
 
-module.exports = function ( options ) {
-  var seneca = this
-  var plugin = 'seneca-auth-token-plugin'
-  options = _.extend({}, default_options, options)
+// default configuration
+var default_options = require('./default-options.js')
 
-  function setToken(args, cb){
+module.exports = function ( seneca, options ) {
+
+  options = _.extend({}, default_options, options || {})
+
+  function set_token ( args, cb ) {
     var tokenkey  = args.tokenkey || options.tokenkey
     var token     = args.token
     var res       = this.fixedargs.res$
     var req       = this.fixedargs.req$
 
-    if (!res.seneca.cookies){
-      res.seneca.cookies = new Cookies(req,res)
+    if ( !res.seneca.cookies ) {
+      res.seneca.cookies = new Cookies( req, res )
     }
 
     res.seneca.cookies.set( tokenkey, token )
 
-    cb(null, {token: token})
+    cb( null, {token: token} )
   }
 
-  function getToken(args, cb){
+
+  function get_token( args, cb ) {
     var tokenkey  = args.tokenkey || options.tokenkey
     var res       = this.fixedargs.res$
     var req       = this.fixedargs.req$
 
-    if (!req.seneca.cookies){
-      req.seneca.cookies = new Cookies(req,res)
+    if ( !req.seneca.cookies ) {
+      req.seneca.cookies = new Cookies( req, res )
     }
 
-    cb(null, {token: req.seneca.cookies.get( tokenkey )})
+    cb( null, {token: req.seneca.cookies.get( tokenkey )} )
   }
 
-  seneca.add({role: 'auth', set: 'token'}, setToken)
-  seneca.add({role: 'auth', get: 'token'}, getToken)
 
-  return {
-    name:plugin
-  }
+  seneca.add( {role: 'auth', set: 'token'}, set_token )
+  seneca.add( {role: 'auth', get: 'token'}, get_token )
 }
